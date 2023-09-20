2b4221d8d872e10e2c9576c246e340b928a004ad
Валерій Перепелица <valerii.perepelytsia@varus.ua>
2023-09-20 09:07:22 +0300

    Init

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        double radius = 5.0;
+
+        MyArea.areaOfCircle(radius);
+    }
+}
---
+++ src/MyArea.java
@@ -0,0 +1,8 @@
+public class MyArea {
+    public static final double PI = 3.14;
+
+    public static void areaOfCircle(double radius) {
+        double area = PI * radius * radius;
+        System.out.println("Площа кола з радіусом " + radius + " дорівнює " + area);
+    }
+}
