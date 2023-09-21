9be91dec5a3517aacd01ca77ec30461bd1f5f269
Валерій Перепелица <valerii.perepelytsia@varus.ua>
2023-09-21 12:57:55 +0300

    Update

--- src/BlackPrint.java
+++ src/BlackPrint.java
@@ -1,5 +1,5 @@
 class BlackPrint extends Printer {
     public void printInBlack(String value) {
-        System.out.println("\u001B[30m" + value + "\u001B[0m"); // Додаємо ANSI-код для чорно-білого кольору
+        System.out.println("\u001B[30m" + value + "\u001B[0m");
     }
 }
