9f66a9c5be1d5abe6d7278e1517e2df6efb44216
Valerii Perepelytsia <perepelitsavn@ukr.net>
2023-10-12 23:52:45 +0300

    Rework

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 class Main {
     public static void main(String[] args) {
-        Vehicles myCar = Vehicles.CAR;
-        System.out.println(myCar);
+        Vehicles.CAR.setColor("Red");
+        Vehicles.MOTORCYCLE.setColor("Black");
+        Vehicles.TRUCK.setColor("Blue");
+
+        System.out.println(Vehicles.CAR);
+        System.out.println(Vehicles.MOTORCYCLE);
+        System.out.println(Vehicles.TRUCK);
     }
 }
--- src/Vehicles.java
+++ src/Vehicles.java
@@ -1,21 +1,25 @@
 public enum Vehicles {
-    CAR(20000, "Red"),
-    MOTORCYCLE(8000, "Black"),
-    TRUCK(35000, "Blue");
+    CAR(20000),
+    MOTORCYCLE(8000),
+    TRUCK(40000);
 
     private int price;
     private String color;
 
-    Vehicles(int price, String color) {
+    Vehicles(int price) {
         this.price = price;
-        this.color = color;
     }
 
     public String getColor() {
         return color;
     }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
     public String toString() {
-        return "Vehicle: " + name() + ", Color: " + color + ", Price: $" + price;
+        return this.name() + " [Color: " + color + ", Price: " + price + "]";
     }
 }
