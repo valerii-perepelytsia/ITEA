f412ab3b7e8bb2a778c9b2778282e89f676fe6c6
Valerii Perepelytsia <perepelitsavn@ukr.net>
2023-10-19 17:11:33 +0300

    Rework

--- src/MyDictionary.java
+++ src/MyDictionary.java
@@ -1,28 +1,42 @@
-import java.util.ArrayList;
-
 public class MyDictionary<K, V> {
-    private ArrayList<K> keys;
-    private ArrayList<V> values;
+    private Node<K, V> head;
+    private int size;
+
+    private static class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V> next;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     public MyDictionary() {
-        keys = new ArrayList<>();
-        values = new ArrayList<>();
+        head = null;
+        size = 0;
     }
 
     public void addElement(K key, V value) {
-        keys.add(key);
-        values.add(value);
+        Node<K, V> newNode = new Node<>(key, value);
+        newNode.next = head;
+        head = newNode;
+        size++;
     }
 
     public V getElement(K key) {
-        int index = keys.indexOf(key);
-        if (index != -1) {
-            return values.get(index);
+        Node<K, V> current = head;
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+            current = current.next;
         }
         return null;
     }
 
-    public final int size() {
-        return keys.size();
+    public int size() {
+        return size;
     }
 }
