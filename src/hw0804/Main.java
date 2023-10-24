6bec039a8a57627b9880b0e9d7cff0d2f4c15faf
Валерій Перепелица <valerii.perepelytsia@varus.ua>
2023-10-24 08:58:48 +0300

    Init

---
+++ src/hw0804/Main.java
@@ -0,0 +1,35 @@
+package hw0804;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int N = 10;
+        LinkedList<Integer> list = getIntegerList(N);
+        System.out.println("Список: " + list);
+        int min = getMinimum(list);
+        System.out.println("Мінімальне число: " + min);
+    }
+
+    public static LinkedList<Integer> getIntegerList(int N) {
+        LinkedList<Integer> list = new LinkedList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < N; i++) {
+            list.add(rand.nextInt(100));
+        }
+        return list;
+    }
+
+    public static int getMinimum(LinkedList<Integer> list) {
+        int min = Integer.MAX_VALUE;
+
+        for (int num : list) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
+    }
+}
