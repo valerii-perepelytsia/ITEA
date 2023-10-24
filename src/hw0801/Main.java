21f623da6359a8126cc9363927b4b5fc5df10de9
Валерій Перепелица <valerii.perepelytsia@varus.ua>
2023-10-24 08:35:18 +0300

    Init

---
+++ src/hw0801/Main.java
@@ -0,0 +1,24 @@
+package hw0801;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("1");
+        myList.add("2");
+        myList.add("3");
+        myList.add("4");
+        myList.add("5");
+        myList.add("6");
+        myList.add("7");
+        myList.add("8");
+
+        Iterator<String> iterator = myList.iterator();
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println(element);
+        }
+    }
+}
