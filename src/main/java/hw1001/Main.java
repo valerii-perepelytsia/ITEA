d4ff3578214fe199bc97f6625037285c3f0b1b94
Valerii Perepelytsia <valerii.perepelytsia@varus.ua>
2023-11-16 10:53:22 +0200

    Fix v3

--- src/main/java/hw1001/Main.java
+++ src/main/java/hw1001/Main.java
@@ -4,15 +4,12 @@ public class Main {
     public static void main(String[] args) {
         MathOperations mathOperations = new MathOperations();
 
-        // Тестування анотацій для додавання та віднімання
         mathOperations.add(5);
         mathOperations.subtract(3);
 
-        // Тестування анотацій для множення та ділення
         mathOperations.multiply(2);
         mathOperations.divide(4);
 
-        // Отримання та виведення результату
         double result = mathOperations.getResult();
         System.out.println("Результат: " + result);
     }
